5e9144b31f66f26c0256efae41e6e6121acc0dbf
josedebuck <debuckjose@gmail.com>
2023-08-31 23:25:29 -0300

    fix problems

---
+++ app/auth/QueryWrapper.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
+import { Toaster } from "react-hot-toast"
+
+const queryClient = new QueryClient()
+
+interface Props {
+    children? : ReactNode
+}
+
+const QueryWrapper = ({children}: Props) => (
+    <QueryClientProvider client={queryClient}>
+        <Toaster />
+        {children}
+        </QueryClientProvider>
+)
+
+export default QueryWrapper;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
+'use client'
 
+import axios from "axios"
+import AddPost from './components/AddPost'
+import {useQuery} from '@tanstack/react-query'
+import Post from "./components/Post"
+import { PostType } from "./types/Posts"
+
+
+// fetch all posts
+const allPosts = async() => {
+  const response = await axios.get("/api/posts/getPosts")
+  return response.data
+}
 
 export default function Home() {
+  const {data, error, isLoading} = useQuery<PostType>({
+    queryFn: allPosts, 
+    queryKey:['posts'],
+  })
+  if(error) return error
+  if (isLoading) return "Loading..."
+
   return (
-    <main className="text-lg py-5">
-      <h1>Ojai seio</h1>
+    <main>
+      <AddPost />
+      {data?.map((post) => (
+        <Post
+          comments={post.Comment} 
+          key={post.id} 
+          name={post.user.name} 
+          avatar={post.user.image} 
+          postTitle={post.title} 
+          id={post.id}
+          />
+      ))}
     </main>
   )
 }
